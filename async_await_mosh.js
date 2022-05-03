ead784e16496b4871f006d12305603ff0d2f858d
Ameya Patil <ameya.patil@mongodb.com>
2022-05-02 17:11:30 -0700

    fixing

--- async_await_mosh.js
+++ async_await_mosh.js
@@ -8,3 +8,14 @@ handleAdd = async () => {
     const obj = { title : 'a', body: 'b' }
     const { data: posts} = await axios.post(apiEndpoint, obj);
 }
+
+// use aync await to set filtered items ?
+// https://devtrium.com/posts/async-functions-useeffect
+useEffect(() => {
+const fetchData = async () => {
+    const response = await fetch(`https://picsum.photos/v2/list?page=1&limit=7`)
+    const json = await response.json()
+    setItems(json)
+}
+fetchData()
+}, [])
